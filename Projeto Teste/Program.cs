e1c62753ee3a74e0dde55e750923a81aee5c2b6f
Willyan Hiroi <whiroi123@gmail.com>
2021-05-20 13:24:34 -0400

    Projeto iniciado, mostrando ola mundo

---
+++ Projeto Teste/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projeto_Teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine(); 
+        }
+    }
+}
